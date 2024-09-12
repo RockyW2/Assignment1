5b5e3b231cf56f736d27fa612cc4e2351cba34f2
Rocky Wang <rocky.wang2@student.tdsb.on.ca>
2024-09-12 11:23:01 -0400

    Fixed bug in Java Feature File

--- javaFeature.java
+++ javaFeature.java
@@ -5,5 +5,14 @@ public class javaFeature {
     public static void main (String args[]){
         System.out.print("Feature has been created");
 
+        int x = 10;
+        int y = 5; 
+        
+        if(x>y){
+            System.out.println("That's True");
+        } else {
+            System.out.println("That's False");
+        }
+
     }
 }
